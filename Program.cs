25053d8fb0f8b2268d4e5fbec0796367da2d8bf0
progamertv1 <missolhenri@gmail.com>
2023-03-18 21:18:25 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Challenge_7;
+class Commands
+{
+    public static void help() {
+        Console.WriteLine("'help' = shows all commands");
+        Console.WriteLine("'stop' = stop the program");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int while0 = 1; //won't work w/o var. (idk why)
+        int while1 = 1;
+        
+        string? input = "a";
+
+        while (while0 == while1) //infinite amount of commands
+        {
+            input = Console.ReadLine();
+            if (input == "help") {
+                Commands.help();
+            }
+            if (input == "stop") {
+                while1 = 0;
+            }
+        }
+    }
+}
+// Make an if-statement for your command then create a method for this command. There are 2 examples.
